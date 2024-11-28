c77b60d57b0d75347e0629a88665302af6c32e53
MauLang18 <maulangbonilla.18@gmail.com>
2024-11-27 23:35:48 -0600

    add new_confirmaciondearribo

--- TrackX.Domain/Entities/DynamicsTrackingLogin.cs
+++ TrackX.Domain/Entities/DynamicsTrackingLogin.cs
@@ -40,6 +40,7 @@ public class DynamicsTrackingLogin
     public object? new_proyecciondeingreso { get; set; }
     public object? new_cbm1 { get; set; }
     public object? new_cantidadteus { get; set; }
+    public object? new_confirmaciondearribo { get; set; }
     public DateTime? createdon { get; set; }
     public string? incidentid { get; set; }
 }
